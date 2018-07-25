1e249e986acd745b43a3a70b87f56596da9d9814
reacheight <reacheight8@gmail.com>
2018-07-25 15:25:04 +0300

    code structure refactored

--- anon-chat.py
+++ anon-chat.py
@@ -9,41 +9,19 @@ chat = Chat(bot)
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    user = message.from_user.id
-    user_state = chat.get_user_state(user)
-
-    if user_state == UserStates.IN_CHAT:
-        pass
-    elif user_state == UserStates.IN_QUEUE:
-        pass
-    else:
-        pass
+    pass
 
 
 @bot.message_handler(commands=['start_chat'])
 def start_chat_command(message):
     user = message.from_user.id
-    user_state = chat.get_user_state(user)
-
-    if user_state == UserStates.IN_CHAT:
-        pass
-    elif user_state == UserStates.IN_QUEUE:
-        pass
-    else:
-        chat.find_interlocutor(user)
+    chat.start(user)
 
 
 @bot.message_handler(commands=['stop'])
 def stop_command(message):
     user = message.from_user.id
-    user_state = chat.get_user_state(user)
-
-    if user_state == UserStates.IN_CHAT:
-        chat.stop_chat(user)
-    elif user_state == UserStates.IN_QUEUE:
-        chat.remove_from_queue(user)
-    else:
-        pass
+    chat.stop(user)
 
 
 @bot.message_handler(content_types=['text'])
--- chat.py
+++ chat.py
@@ -15,7 +15,7 @@ class Chat:
     def set_user_state(self, user, state):
         self.chat_states[user] = state
 
-    def is_ready(self):
+    def interlocutor_is_ready(self):
         return len(self.chat_queue) > 0
 
     def add_to_table(self, first_user, second_user):
@@ -26,8 +26,9 @@ class Chat:
         self.chat_states[first_user] = self.chat_states[second_user] = UserStates.IN_CHAT
         self.add_to_table(first_user, second_user)
 
-    def notify_about_chat(self, user):
-        self.bot.send_message(user, 'Собеседник найден! Начинайте общаться!')
+    def notify_about_chat(self, *users):
+        for user in users:
+            self.bot.send_message(user, 'Собеседник найден! Начинайте общаться!')
 
     def notify_about_queue(self, user):
         self.bot.send_message(user, 'Вы добавлены в очередь. Ожидайте собеседника.')
@@ -41,17 +42,24 @@ class Chat:
     def notify_about_user_leave_queue(self, user):
         self.bot.send_message(user, 'Вы покинули очередь.')
 
+    def get_interlocutor(self):
+        interlocutor = choice(self.chat_queue)
+        self.chat_queue.remove(interlocutor)
+
+        return interlocutor
+
+    def add_to_queue(self, user):
+        self.chat_states[user] = UserStates.IN_QUEUE
+        self.chat_queue.append(user)
+
     def find_interlocutor(self, user):
-        if self.is_ready():
-            interlocutor = choice(self.chat_queue)
-            self.chat_queue.remove(interlocutor)
+        if self.interlocutor_is_ready():
+            interlocutor = self.get_interlocutor()
             self.connect(user, interlocutor)
-            self.notify_about_chat(user)
-            self.notify_about_chat(interlocutor)
+            self.notify_about_chat(user, interlocutor)
 
         else:
-            self.chat_states[user] = UserStates.IN_QUEUE
-            self.chat_queue.append(user)
+            self.add_to_queue(user)
             self.notify_about_queue(user)
 
     def send_message_to_interlocutor(self, user, message):
@@ -76,3 +84,26 @@ class Chat:
         del self.chat_states[user]
         self.chat_queue.remove(user)
         self.notify_about_user_leave_queue(user)
+
+    def start(self, user):
+        user_state = self.get_user_state(user)
+
+        if user_state == UserStates.IN_CHAT:
+            # пишем что юзер уже в чате
+            pass
+        elif user_state == UserStates.IN_QUEUE:
+            # пишем что юзер уже в очереди
+            pass
+        else:
+            self.find_interlocutor(user)
+
+    def stop(self, user):
+        user_state = self.get_user_state(user)
+
+        if user_state == UserStates.IN_CHAT:
+            self.stop_chat(user)
+        elif user_state == UserStates.IN_QUEUE:
+            self.remove_from_queue(user)
+        else:
+            # пишем что юзер не в чате и не в очереди
+            pass
